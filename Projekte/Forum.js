0558fd23fe3377245909ce0eeeb5a132f5387427
Bibs628 <45543748+Bibs628@users.noreply.github.com>
2024-12-06 08:22:11 +0100

    anpassungen unfug & Bilder

--- Projekte/Forum.js
+++ Projekte/Forum.js
@@ -1,3 +1,29 @@
+//NavBar
+function hideIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide");
+}
+
+function showIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:block;");
+    navigation.classList.add("hide");
+}
+
+//Comment
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+//Reply
+function showReply(){
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
 
 document.getElementById('forumForm').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -29,4 +55,4 @@ function displayPosts() {
     });
 }
 
-    document.addEventListener('DOMContentLoaded', displayPosts);
+document.addEventListener('DOMContentLoaded', displayPosts);
