75b6cfabbf4e7bb2b0c428cb7f25d70f48a2efe5
Bibs628 <45543748+Bibs628@users.noreply.github.com>
2024-12-07 15:17:06 +0100

    testpush on live site

--- Projekte.js
+++ Projekte.js
@@ -33,7 +33,7 @@ if (storedProjects) {
     createProjectElements(JSON.parse(storedProjects));
 } else {
     // Projektdaten laden und mischen
-    fetch("./JSON/Projekte.json")
+    fetch("JSON/Projekte.json")
         .then(response => response.json())
         .then(data => {
             const shuffledData = shuffle(data);
@@ -51,23 +51,3 @@ document.getElementById('sortButton').addEventListener('click', function() {
     localStorage.setItem('shuffledProjects', JSON.stringify(projects));
     createProjectElements(projects);
 });
-
-/*
-    let parentDiv = document.getElementById("projectsjs");
-
-let data = fetch("./JSON/Projekte.json")
-    .then((response) => response.json())
-    .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            let projectDiv = document.createElement("div");
-            projectDiv.className = "Projekte";
-            projectDiv.innerHTML = `<a href="${data[i].link}">
-            <img src="${data[i].image}" alt="${data[i].alt}" width="80%" height="80%">
-            <h3>${data[i].name}</h3>
-            <p>
-            ${data[i].description}
-            </p></a>`;
-            parentDiv.appendChild(projectDiv);
-        }
-    }).catch();
-    */
